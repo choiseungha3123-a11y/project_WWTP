f5aa22ba6cabd832154a141c427c0eb83aad9005
sujidaddy <kyuhuhu.sujidaddy@gmail.com>
2026-02-26 13:11:54 +0900

    no message

--- Spring/aiprojectserver/src/main/java/kr/kro/prjectwwtp/config/SwaggerConfig.java
+++ Spring/aiprojectserver/src/main/java/kr/kro/prjectwwtp/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package kr.kro.prjectwwtp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -34,6 +36,16 @@ public class SwaggerConfig {
 				.title("Project WWTP") // 제목
 				.description("API 리스트 입니다.")	// 설명
 				.version("1.0.0");	// 버전
+	}
+	
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/v3/api-docs/**").allowedOrigins("*");
+	            registry.addMapping("/swagger-ui/**").allowedOrigins("*");
+	        }
+	    };
 	}
 
 }
